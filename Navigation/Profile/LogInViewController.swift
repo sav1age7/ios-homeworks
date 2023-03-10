1482b5d168c2c351f8d70eec466a476f23fd42b5
Vladislav <extsea17@icloud.com>
2023-03-10 21:50:04 +0500

    2.3 part 1, LogInViewController - ready

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LogInViewController: UIViewController, UITextFieldDelegate {
+class LogInViewController: UIViewController {
     // MARK: - Constants & variables
     private let logo: UIImageView = {
         let logo = UIImageView(image: UIImage(named: "logo"))
@@ -48,12 +48,13 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         mailPhoneText.leftViewMode = .always
         mailPhoneText.textColor = .black
         mailPhoneText.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        mailPhoneText.tintColor = UIColor(named: "AccentColor")
+        mailPhoneText.backgroundColor = .systemGray6
         mailPhoneText.autocapitalizationType = .none
         mailPhoneText.autocorrectionType = .no
         mailPhoneText.returnKeyType = .next
         mailPhoneText.keyboardType = .default
         mailPhoneText.delegate = self
+        mailPhoneText.tag = 0
         return mailPhoneText
     }()
     
@@ -65,14 +66,16 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         passwordText.isSecureTextEntry = true
         passwordText.textColor = .black
         passwordText.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        passwordText.tintColor = UIColor(named: "AccentColor")
+        passwordText.backgroundColor = .systemGray6
         passwordText.autocapitalizationType = .none
         passwordText.autocorrectionType = .no
         passwordText.returnKeyType = .done
         passwordText.keyboardType = .default
         passwordText.delegate = self
+        passwordText.tag = 1
         return passwordText
     }()
+    
     private let divider: UIView = {
         let borderForStack = UIView()
         borderForStack.layer.borderWidth = 0.5
@@ -100,16 +103,27 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         onTapToHideKeyboard()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupKeyboardObservers()
+        addLoginButtonAction()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
+    }
+    
     private func addSubviews() {
-        view.addSubview(logo)
-        view.addSubview(mailPhoneText)
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
+        contentView.addSubview(logo)
         contentView.addSubview(vStack)
         vStack.addArrangedSubview(mailPhoneText)
         vStack.addArrangedSubview(divider)
         vStack.addArrangedSubview(passwordText)
         view.addSubview(loginButton)
+        
     }
     
     private func setupUI() {
@@ -121,15 +135,10 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         let safeArea = view.safeAreaLayoutGuide
         
         NSLayoutConstraint.activate([
-            logo.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 120),
-            logo.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-            logo.widthAnchor.constraint(equalToConstant: 100),
-            logo.heightAnchor.constraint(equalToConstant: 100),
-            
-            scrollView.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 120),
-            scrollView.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 16),
-            scrollView.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -16),
-            scrollView.heightAnchor.constraint(equalToConstant: 100),
+            scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 0),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -100),
             
             contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
             contentView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
@@ -137,21 +146,72 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
             contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-            
-            vStack.topAnchor.constraint(equalTo: contentView.topAnchor),
+
+            logo.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 120),
+            logo.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            logo.widthAnchor.constraint(equalToConstant: 100),
+            logo.heightAnchor.constraint(equalToConstant: 100),
+           
+            vStack.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 120),
             vStack.leftAnchor.constraint(equalTo: contentView.leftAnchor),
             vStack.rightAnchor.constraint(equalTo: contentView.rightAnchor),
-            vStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            vStack.heightAnchor.constraint(equalToConstant: 100),
 
             divider.heightAnchor.constraint(equalToConstant: 1),
             
-            loginButton.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 16),
+            loginButton.topAnchor.constraint(equalTo: vStack.bottomAnchor, constant: 16),
             loginButton.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 16),
             loginButton.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -16),
             loginButton.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
     
+    func setupKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(self.willShowKeyboard(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(self.willHideKeyboard(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    func removeKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.removeObserver(self)
+    }
+    
+    func addLoginButtonAction() {
+        loginButton.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
+    }
+    
+    @objc func loginButtonPressed() {
+        let nextController = ProfileViewController()
+        navigationController?.pushViewController(nextController, animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextTag = textField.tag + 1
+        let nextResponder = textField.superview?.viewWithTag(nextTag) as? UIResponder
+        if nextResponder != nil {
+            nextResponder?.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return false
+    }
+}
+
+// MARK: - Extensions
+extension LogInViewController: UITextFieldDelegate {
     private func onTapToHideKeyboard() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(tap)
@@ -162,7 +222,25 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         self.vStack.arrangedSubviews.forEach { subview in
             if subview.isFirstResponder {
                 subview.resignFirstResponder()
+                resetScrollViewInsetBottom()
             }
         }
+    }
+    
+    func resetScrollViewInsetBottom() {
+        scrollView.contentInset.bottom = 0
+    }
+    
+    @objc func willShowKeyboard(_ notification: NSNotification) {
+        let keyboardHeigth = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height
+        
+        let diff = view.bounds.height - (keyboardHeigth ?? 0) - loginButton.frame.maxY
+        if diff < 0 {
+            scrollView.contentInset.bottom = 0 - diff
+        }
+    }
+    
+    @objc func willHideKeyboard(_ notification: NSNotification) {
+        resetScrollViewInsetBottom()
     }
 }
